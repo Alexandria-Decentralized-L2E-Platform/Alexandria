0f248428b1a00c76026f34cd880237b76a9cbc06
mattyu <www.yukawai@gmail.com>
2023-08-03 11:16:37 -0400

    test

--- torontohackathon/src/App.test.tsx
+++ torontohackathon/src/App.test.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
@@ -7,3 +6,4 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+//test
