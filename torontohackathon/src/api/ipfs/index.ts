ee81a0fbda83bbb5e9d336dde6851040630e0e3c
Daniel So Ho Kit <58765171+Daniel320So@users.noreply.github.com>
2023-08-10 18:16:42 -0400

    add ipfs function

--- torontohackathon/src/api/ipfs/index.ts
+++ torontohackathon/src/api/ipfs/index.ts
@@ -1,35 +1,31 @@
 // IPFS Pinata API Gateway
 
-// import config from "./config.js";
-// import api from "api";
-// import fs from "fs";
+import { config } from './config';
 
-// const sdk = api('@pinata-cloud/v1.0#fc5xxr21ljszoswo');
-// sdk.auth(config.JWT);
+interface IpinJSONtoIPFSResponseData {
+  IpfsHash: string;
+  PinSize: number;
+  Timestamp: string;
+}
 
-// interface IpinJSONtoIPFSResponseData {
-//   IpfsHash: string,
-//   PinSize: number,
-//   Timestamp: string
-// };
+export const pinJSONtoIPFS = async (obj: object): Promise<IpinJSONtoIPFSResponseData> => {
+  const options = {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'content-type': 'application/json',
+      authorization: 'Bearer ' + config.JWT,
+    },
+    body: JSON.stringify({
+      pinataContent: obj,
+    }),
+  };
 
-// const pinJSONtoIPFS = async(obj: object):Promise<IpinJSONtoIPFSResponseData> => {
-//   const response = await sdk.postPinningPinjsontoipfs({
-//     pinataContent: obj
-//   });
-//   await readJSONFromIPFS(response.data.IpfsHash);
-//   return response.data
-// };
+  const response = await fetch('https://api.pinata.cloud/pinning/pinJSONToIPFS', options);
+  return response.json();
+};
 
-// const pinFiletoIPFS = async(filePath: string):Promise<IpinJSONtoIPFSResponseData> => {
-//   const fileStream = fs.createReadStream(filePath);
-//   const response = await sdk.postPinningPinfiletoipfs({
-//     file: fileStream
-//   })
-//   return response;
-// }
-
-export const readJSONFromIPFS = async(cid:string): Promise<any> => {
-  const request = await fetch("https://gateway.pinata.cloud/ipfs/" + cid)
+export const readJSONFromIPFS = async (cid: string): Promise<any> => {
+  const request = await fetch('https://gateway.pinata.cloud/ipfs/' + cid);
   return request;
 };
