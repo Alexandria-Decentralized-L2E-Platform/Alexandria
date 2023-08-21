8a4899548f30fa6d742cf1389952af049c064088
mattyu <www.yukawai@gmail.com>
2023-08-21 18:30:46 -0400

    .

--- dapp/src/api/index.ts
+++ dapp/src/api/index.ts
@@ -1,6 +1,6 @@
-import * as ipfs from './ipfs';
-import * as contracts from './contracts';
 import * as blockchain from './blockchain';
+import * as contracts from './contracts';
+import * as ipfs from './ipfs';
 
 // Program interface
 export interface IProgram extends contracts.IProgram, ipfs.IProgramObjectIPFS {}
@@ -42,6 +42,7 @@ enum topic {
   MusicNFT = 'Music NFT',
   InfraAPI = 'Infra / API',
   Crosschain = 'Crosschain',
+  Chain = 'Chain',
 }
 
 enum type {
@@ -49,4 +50,4 @@ enum type {
   Article = 'Article',
 }
 
-export { ipfs, contracts, blockchain, getProgramById, getAllPrograms, topic, type };
+export { blockchain, contracts, getAllPrograms, getProgramById, ipfs, topic, type };
